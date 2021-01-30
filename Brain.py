834d741c328277bacc56a871891e935e7d874163
maiiasleptcova <mayyasleptsova@gmail.com>
2021-01-30 18:15:57 +0000

    adding files for ML project

---
+++ Brain.py
@@ -0,0 +1,16 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+print(execution_path)
+
+prediction = ImageClassification()
+
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "godzilla.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
